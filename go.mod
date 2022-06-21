b20a94c15cdf47ef34349ee6cd652d9109cec963
Alex Parlett <alexparlett@gmail.com>
2022-06-21 14:06:46 +0100

    Support sbus auth mechanism via commands
    * support commands instead of special routing key
    * support saving new user in consul
    * add more verbose export info

--- go.mod
+++ go.mod
@@ -3,17 +3,37 @@ module github.com/servehub/sbus-cli
 go 1.17
 
 require (
-	github.com/TylerBrock/colorjson v0.0.0-20180527164720-95ec53f28296
-	github.com/streadway/amqp v0.0.0-20190827072141-edfb9018d271
+	github.com/TylerBrock/colorjson v0.0.0-20200706003622-8a50f05110d2
+	github.com/hashicorp/consul/api v1.11.0
+	github.com/streadway/amqp v1.0.0
 	gopkg.in/alecthomas/kingpin.v2 v2.2.6
 )
 
 require (
 	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
 	github.com/alecthomas/units v0.0.0-20190924025748-f65c72e2690d // indirect
-	github.com/fatih/color v1.7.1-0.20180516100307-2d684516a886 // indirect
+	github.com/armon/go-metrics v0.3.10 // indirect
+	github.com/fatih/color v1.9.0 // indirect
+	github.com/google/btree v1.0.0 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.1 // indirect
+	github.com/hashicorp/go-hclog v0.14.1 // indirect
+	github.com/hashicorp/go-immutable-radix v1.3.0 // indirect
+	github.com/hashicorp/go-msgpack v0.5.5 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/hashicorp/go-rootcerts v1.0.2 // indirect
+	github.com/hashicorp/go-sockaddr v1.0.2 // indirect
+	github.com/hashicorp/go-uuid v1.0.2 // indirect
+	github.com/hashicorp/golang-lru v0.5.4 // indirect
+	github.com/hashicorp/memberlist v0.3.1 // indirect
+	github.com/hashicorp/serf v0.9.7 // indirect
 	github.com/hokaccha/go-prettyjson v0.0.0-20211117102719-0474bc63780f // indirect
-	github.com/mattn/go-colorable v0.1.0 // indirect
-	github.com/mattn/go-isatty v0.0.4 // indirect
-	golang.org/x/sys v0.0.0-20180616030259-6c888cc515d3 // indirect
+	github.com/mattn/go-colorable v0.1.6 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/mitchellh/go-testing-interface v1.14.0 // indirect
+	github.com/mitchellh/mapstructure v1.4.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+	golang.org/x/net v0.0.0-20211216030914-fe4d6282115f // indirect
+	golang.org/x/sys v0.0.0-20220412211240-33da011f77ad // indirect
 )
--- main.go
+++ main.go
@@ -15,146 +15,230 @@ import (
 	"encoding/hex"
 
 	"github.com/TylerBrock/colorjson"
+	"github.com/hashicorp/consul/api"
 	"github.com/streadway/amqp"
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
-var version = "1.0"
+var (
+	version = "1.0"
+	app     = kingpin.New("sbus", "A command-line interface to sbus.")
+	envName = app.Flag("env", "Environment: qa, stage, live").Default("local").String()
+
+	register       = app.Command("register", "Register a new user.")
+	consul         = register.Flag("save-consul", "Save user to consul?").Default("false").Bool()
+	publicKeyPath  = register.Flag("public-key-path", "Where the public keys are on consul").Default("services/keys/public/").String()
+	identitiesPath = register.Flag("identities-path", "Where the identities are on consul").Default("sbus/rbac/identities/").String()
+	groups         = register.Flag("group", "Group for user.").Strings()
+	registerName   = register.Arg("name", "Name of user.").Required().String()
+
+	send        = app.Command("Send", "Send a message to the service bus.")
+	isEvent     = send.Flag("event", "Is it event?").Default("false").Bool()
+	routingKey  = send.Arg("routing-key", "Routing key").Required().String()
+	requestBody = send.Arg("request-body", "Request JSON body").Required().String()
+)
+
+type ConsulPublicKey struct {
+	PublicKey string `json:"publicKey"`
+}
 
 func main() {
-	routingKey := kingpin.Arg("routing-key", "Routing key").Required().String()
-	requestBody := kingpin.Arg("request-body", "Request JSON body").Required().String()
-	envName := kingpin.Flag("env", "Environment: qa, stage, live").Default("local").String()
-	isEvent := kingpin.Flag("event", "Is it event?").Default("false").Bool()
-
 	kingpin.Version(version)
-	kingpin.Parse()
 
-	if *routingKey == "new-user" {
+	switch kingpin.MustParse(app.Parse(os.Args[1:])) {
+	// Register user
+	case register.FullCommand():
 		pubKey, privKey, _ := ed25519.GenerateKey(nil)
 
-		println(*requestBody)
-		println("pub:", hex.EncodeToString(pubKey))
-		println("priv:", hex.EncodeToString(privKey.Seed()))
-		return
-	}
+		hexEncodedPublicKEy := hex.EncodeToString(pubKey)
 
-	amqpUrl, ok := os.LookupEnv("SBUS_AMQP_" + strings.ToUpper(*envName) + "_URL")
-	if !ok {
-		amqpUrl = "amqp://guest:guest@localhost:5672/"
-	}
+		userKey := "users/" + *registerName
 
-	connection, err := amqp.Dial(amqpUrl)
-	if err != nil {
-		log.Panicf("Dial: %s", err)
-	}
-	defer connection.Close()
-
-	channel, err := connection.Channel()
-	if err != nil {
-		log.Panicf("Channel: %s", err)
-	}
-
-	replyQueue, err := channel.QueueDeclare(
-		"",    // name of the queue
-		false, // durable
-		true,  // delete when unused
-		true,  // exclusive
-		false, // noWait
-		nil,   // arguments
-	)
-	if err != nil {
-		log.Panicf("Queue Declare: %s", err)
-	}
-
-	deliveries, err := channel.Consume(
-		replyQueue.Name, // name
-		"",              // consumerTag,
-		false,           // noAck
-		false,           // exclusive
-		false,           // noLocal
-		false,           // noWait
-		nil,             // arguments
-	)
-	if err != nil {
-		log.Panicf("Queue Consume: %s", err)
-	}
-
-	exchange := "sbus.common"
-	replyTo := replyQueue.Name
-
-	if *isEvent {
-		exchange = "sbus.events"
-		replyTo = ""
-	}
-
-	rand.Seed(time.Now().UnixNano())
-
-	payload := []byte(`{"body":` + *requestBody + `}`)
-
-	headers := amqp.Table{
-		"correlation-id": randString(32),
-		"expired-at":     time.Now().Add(time.Minute * 5).UnixMilli(),
-	}
-
-	if privateKeyHex, ok := os.LookupEnv("SBUS_PRIVATE_KEY"); ok {
-		if privateKey, err := hex.DecodeString(privateKeyHex); err == nil {
-			pvk := ed25519.NewKeyFromSeed(privateKey)
-			sigb := ed25519.Sign(pvk, payload)
-
-			if user, ok := os.LookupEnv("SBUS_USER"); ok {
-				headers["origin"] = user
-			}
-
-			headers["signature"] = base64.URLEncoding.EncodeToString(sigb)
+		if *consul {
+			ConfigureUserInConsul(hexEncodedPublicKEy, userKey)
 		}
-	}
 
-	if err = channel.Publish(
-		exchange,    // publish to an exchange
-		*routingKey, // routing to 0 or more queues
-		false,       // mandatory
-		false,       // immediate
-		amqp.Publishing{
-			Headers:      headers,
-			Body:         payload,
-			DeliveryMode: amqp.Transient, // 1=non-persistent, 2=persistent
-			Priority:     0,              // 0-9
-			ReplyTo:      replyTo,
-		},
-	); err != nil {
-		log.Panicf("Exchange Publish: %s", err)
-	}
+		println("add to env SBUS_USER=" + userKey)
+		println("add to env SBUS_" + strings.ToUpper(*envName) + "_PRIVATE_KEY=" + hex.EncodeToString(privKey.Seed()))
+		println("add to env SBUS_" + strings.ToUpper(*envName) + "_PUBLIC_KEY=" + hexEncodedPublicKEy)
+		return
 
-	if !*isEvent {
-		for d := range deliveries {
-			var response map[string]interface{}
-			json.Unmarshal(d.Body, &response)
+	// Post message
+	case send.FullCommand():
+		amqpUrl, ok := os.LookupEnv("SBUS_AMQP_" + strings.ToUpper(*envName) + "_URL")
+		if !ok {
+			amqpUrl = "amqp://guest:guest@localhost:5672/"
+		}
 
-			f := colorjson.NewFormatter()
-			f.Indent = 2
+		connection, err := amqp.Dial(amqpUrl)
+		if err != nil {
+			log.Panicf("Dial: %s", err)
+		}
+		defer connection.Close()
 
-			jsonStr, err := f.Marshal(response["body"])
-			if err != nil {
-				log.Panicf("Error parse response: %s", err)
+		channel, err := connection.Channel()
+		if err != nil {
+			log.Panicf("Channel: %s", err)
+		}
+
+		replyQueue, err := channel.QueueDeclare(
+			"",    // name of the queue
+			false, // durable
+			true,  // delete when unused
+			true,  // exclusive
+			false, // noWait
+			nil,   // arguments
+		)
+		if err != nil {
+			log.Panicf("Queue Declare: %s", err)
+		}
+
+		deliveries, err := channel.Consume(
+			replyQueue.Name, // name
+			"",              // consumerTag,
+			false,           // noAck
+			false,           // exclusive
+			false,           // noLocal
+			false,           // noWait
+			nil,             // arguments
+		)
+		if err != nil {
+			log.Panicf("Queue Consume: %s", err)
+		}
+
+		exchange := "sbus.common"
+		replyTo := replyQueue.Name
+
+		if *isEvent {
+			exchange = "sbus.events"
+			replyTo = ""
+		}
+
+		rand.Seed(time.Now().UnixNano())
+
+		payload := []byte(`{"body":` + *requestBody + `}`)
+
+		headers := amqp.Table{
+			"correlation-id": RandString(32),
+			"expired-at":     time.Now().Add(time.Minute * 5).UnixMilli(),
+		}
+
+		if privateKeyHex, ok := os.LookupEnv("SBUS_" + strings.ToUpper(*envName) + "_PRIVATE_KEY"); ok {
+			if privateKey, err := hex.DecodeString(privateKeyHex); err == nil {
+				pvk := ed25519.NewKeyFromSeed(privateKey)
+				sigb := ed25519.Sign(pvk, payload)
+
+				if user, ok := os.LookupEnv("SBUS_USER"); ok {
+					headers["origin"] = user
+				}
+
+				headers["signature"] = base64.URLEncoding.EncodeToString(sigb)
 			}
+		}
 
-			fmt.Printf("\n%s\n\n%s\n\n", response["status"], jsonStr)
+		if err = channel.Publish(
+			exchange,    // publish to an exchange
+			*routingKey, // routing to 0 or more queues
+			false,       // mandatory
+			false,       // immediate
+			amqp.Publishing{
+				Headers:      headers,
+				Body:         payload,
+				DeliveryMode: amqp.Transient, // 1=non-persistent, 2=persistent
+				Priority:     0,              // 0-9
+				ReplyTo:      replyTo,
+			},
+		); err != nil {
+			log.Panicf("Exchange Publish: %s", err)
+		}
 
-			d.Ack(false)
+		if !*isEvent {
+			for d := range deliveries {
+				var response map[string]interface{}
+				json.Unmarshal(d.Body, &response)
 
-			if status, err2 := strconv.Atoi(fmt.Sprintf("%s", response["status"])); err2 != nil || status >= 400 {
-				os.Exit(2)
-			} else {
-				os.Exit(0)
+				f := colorjson.NewFormatter()
+				f.Indent = 2
+
+				jsonStr, err := f.Marshal(response["body"])
+				if err != nil {
+					log.Panicf("Error parse response: %s", err)
+				}
+
+				fmt.Printf("\n%s\n\n%s\n\n", response["status"], jsonStr)
+
+				d.Ack(false)
+
+				if status, err2 := strconv.Atoi(fmt.Sprintf("%s", response["status"])); err2 != nil || status >= 400 {
+					os.Exit(2)
+				} else {
+					os.Exit(0)
+				}
 			}
 		}
 	}
 }
 
+func ConfigureUserInConsul(hexEncodedPublicKEy string, userKey string) {
+	if *envName == "local" {
+		log.Printf("Consul cannot be configured for %s", *envName)
+		return
+	}
+
+	consulUrl, ok := os.LookupEnv("SBUS_CONSUL_" + strings.ToUpper(*envName) + "_URL")
+	if !ok {
+		log.Panicf("Consul not configured for %s", *envName)
+	}
+
+	consulDatacenter, ok := os.LookupEnv("SBUS_CONSUL_" + strings.ToUpper(*envName) + "_DC")
+	if !ok {
+		log.Panicf("Consul datacenter not configured for %s", *envName)
+	}
+
+	config := api.DefaultConfig()
+	config.Address = consulUrl
+	config.Datacenter = consulDatacenter
+
+	client, _ := api.NewClient(config)
+
+	writeOptions := api.WriteOptions{}
+
+	publicKey := ConsulPublicKey{PublicKey: hexEncodedPublicKEy}
+
+	marshal, err := json.Marshal(publicKey)
+	if err != nil {
+		log.Panicf("Couldn't serialise publicKey to json: %s", err)
+	}
+
+	publicKeyKVPair := api.KVPair{
+		Key:   *publicKeyPath + userKey,
+		Value: marshal,
+	}
+
+	_, err = client.KV().Put(&publicKeyKVPair, &writeOptions)
+	if err != nil {
+		log.Panicf("Consul Public Key Put: %s", err)
+	}
+
+	marshal, err = json.Marshal(*groups)
+	if err != nil {
+		log.Panicf("Couldn't serialise groups to json: %s", err)
+	}
+
+	identityKeyPair := api.KVPair{
+		Key:   *identitiesPath + userKey,
+		Value: marshal,
+	}
+
+	_, err = client.KV().Put(&identityKeyPair, &writeOptions)
+	if err != nil {
+		log.Panicf("Consul Identity Key Put: %s", err)
+	}
+}
+
 var letters = []rune("abcdefghijklmnopqrstuvwxyz0123456789")
 
-func randString(n int) string {
+func RandString(n int) string {
 	b := make([]rune, n)
 	for i := range b {
 		b[i] = letters[rand.Intn(len(letters))]
